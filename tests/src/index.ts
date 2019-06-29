8eddadb2b0d7299ffbac58019469d8d20a7c07e9
Sam Lanning <sam@samlanning.com>
2019-06-28 22:56:20 -0700

    Add tests for headers and footers

--- tests/src/index.ts
+++ tests/src/index.ts
@@ -7,6 +7,7 @@ import * as html from 'remark-html';
 import * as codeExtra from 'remark-code-extra';
 import { Options } from 'remark-code-extra/options';
 import { MDASTCode } from 'remark-code-extra/types';
+import { Element } from 'remark-code-extra/types/hast';
 import {promisify} from 'util';
 
 const readFile = promisify(fs.readFile);
@@ -41,6 +42,15 @@ function test(name: string, input: string, output: string, options: Options) {
   });
 }
 
+const element: Element = {
+  type: 'element',
+  tagName: 'span',
+  properties: {
+    className: ['some-thing']
+  },
+  children: []
+};
+
 describe('main tests', () => {
   test('Skip all', 'basic', '001', {
     transform: () => null
@@ -57,4 +67,43 @@ describe('main tests', () => {
   test('No transformation', 'basic', '003', {transform: {
     // No concrete transformation
   }});
+  test('Add header', 'basic', '004', {transform: {
+    headers: [element]
+  }
+  });
+  test('Add header func', 'basic', '004', {
+    transform: () => ({
+      headers: [element]
+    })
+  });
+  test('Add header async', 'basic', '004', {
+    transform: async () => ({
+      headers: [element]
+    })
+  });
+  test('Add multiple headers', 'basic', '005', {
+    transform: {
+      headers: [element, element]
+    }
+  });
+  test('Add footer', 'basic', '006', {
+    transform: {
+      footers: [element]
+    }
+  });
+  test('Add footer func', 'basic', '006', {
+    transform: () => ({
+      footers: [element]
+    })
+  });
+  test('Add footer async', 'basic', '006', {
+    transform: async () => ({
+      footers: [element]
+    })
+  });
+  test('Add multiple footers', 'basic', '007', {
+    transform: {
+      footers: [element, element]
+    }
+  });
 });
