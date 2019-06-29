9584d0c118ceebd34a03e3341aeac4d5b2149bed
Sam Lanning <sam@samlanning.com>
2019-06-28 22:40:53 -0700

    Finish implementation

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
 import {Attacher, Transformer} from 'unified';
 import visit = require('unist-util-visit');
 
-import {validateOptions} from './options';
+import { validateOptions } from './options';
 import { MDASTCode, MDASTCodeExtra } from './types';
+import { Element, Text } from './types/hast';
 
 const attacher: Attacher = (options) =>  {
   if (!validateOptions(options))
@@ -18,11 +19,47 @@ const attacher: Attacher = (options) =>  {
       const transform = typeof options.transform === 'function' ? options.transform(node) : options.transform;
       // Asyncronously apply the transformation
       transformations.push(
-        Promise.resolve(transform).then(transform => {
+        Promise.resolve(transform).then(async transform => {
           if (transform) {
+            // get code child html
+            const codeChildren: (Element | Text)[] =
+              node.data && (node.data.hChildren as (Element | Text)[]) ||
+              [{
+                type: 'text',
+                value: node.value
+              }];
+            const codeProperties: any =
+              node.data && node.data.hProperties ||
+              node.lang ? {
+                className: ['language-' + node.lang]
+              } : {};
             // apply transformation
             const n = node as MDASTCodeExtra;
             n.type = 'code-extra';
+            if (!n.data) n.data = {};
+            const headers = transform.headers ? await Promise.resolve(transform.headers) : [];
+            const footers = transform.footers ? await Promise.resolve(transform.footers) : [];
+            const children: Element[] = [
+              ...headers,
+              {
+                type: 'element',
+                tagName: 'pre',
+                children: [
+                  {
+                    type: 'element',
+                    tagName: 'code',
+                    properties: codeProperties,
+                    children: codeChildren
+                  }
+                ]
+              },
+              ...footers
+            ];
+            n.data.hName = 'div';
+            n.data.hProperties = {
+              className: ['code-extra']
+            };
+            n.data.hChildren = children;
           }
         })
       );
